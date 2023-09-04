67dd7eeac68c42876775b56ea0c3869ea35ee80b
jabu04 <mslserp.website3.ph@gmail.com>
2023-09-05 04:13:20 +0800

    dashboard transact / best selller

---
+++ js/adminJS/dashboard.js
@@ -0,0 +1,6 @@
+import { get, getDatabase, ref, set, child, update, remove, push } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-database.js";
+import { signInWithEmailAndPassword, getAuth, signOut, onAuthStateChanged, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-auth.js";
+const db = getDatabase();
+const dbref = ref(db);
+const auth = getAuth();
+
