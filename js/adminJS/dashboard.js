242f8fbb54a03243b61ce9b9f3d47fae41628925
jabu04 <mslserp.website3.ph@gmail.com>
2023-09-08 05:52:53 +0800

    transaction

--- js/adminJS/dashboard.js
+++ js/adminJS/dashboard.js
@@ -40,6 +40,53 @@ onValue(child(dbref, `Transactions/${theYear}/${theMonth+1}/${theDate}/1/`),(sna
 },{
 
 });
+onValue(child(dbref, `Transactions/${theYear}/${theMonth+1}/${theDate}/1/`),(snapchat) => {
+    var html = "";
+    var total = 0;
+    var quan = 0;
+    snapchat.forEach(element => {
+        element.forEach(data =>{
+            
+          data.forEach(data2 =>{
+            quan += parseInt(data2.val().productQty)
+          
+            html += `<li class="d-flex mb-3 pb-1 align-items-center">
+                        <div class="badge bg-label-success rounded me-3 p-2">
+                            <i class="ti ti-browser-check ti-sm"></i>
+                        </div>
+                        <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+                            <div class="me-2">
+                                <h6 class="mb-0">${data.key}</h6>
+                                <small class="text-muted d-block">${data2.val().product} Qty: ${data2.val().productQty}</small>
+                            </div>
+                            <div class="user-progress d-flex align-items-center gap-1">
+                                <h6 class="mb-0 text-success">+₱ ${parseInt(data2.val().productTotal.replace('₱ ', ''))}</h6>
+                            </div>
+                        </div>
+                    </li>`;
+            
 
+          })
+        })
+    });
+
+    $('.transactionContent').html(html);
+},{
+
+});
+{/* <li class="d-flex mb-3 pb-1 align-items-center">
+    <div class="badge bg-label-success rounded me-3 p-2">
+        <i class="ti ti-browser-check ti-sm"></i>
+    </div>
+    <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+        <div class="me-2">
+            <h6 class="mb-0">Bank Transfer</h6>
+            <small class="text-muted d-block">Add Money</small>
+        </div>
+        <div class="user-progress d-flex align-items-center gap-1">
+            <h6 class="mb-0 text-success">+₱480</h6>
+        </div>
+    </div>
+</li> */}
 
 
