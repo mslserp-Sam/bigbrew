40ce09c751e4d9298794892b17e8fbebaa48a813
jabu04 <mslserp.website3.ph@gmail.com>
2023-09-08 01:30:47 +0800

    dashboard transact

--- js/adminJS/dashboard.js
+++ js/adminJS/dashboard.js
@@ -10,16 +10,36 @@ const theYear = date.getFullYear();
 const theMonth = date.getMonth();
 const theDate = date.getDate();
 const theDay = date.getDay();
-onValue(child(dbref, `Transactions/${theYear}/${theMonth+1}/${theDate}/1/`),(snapchat) => {
-    snapchat.forEach(element => {
+// onValue(child(dbref, `Transactions/${theYear}/${theMonth+1}/${theDate}/1/`),(snapchat) => {
+//     snapchat.forEach(element => {
      
-        $('#sales').text(element.val().Sales)
-        $('#cups').text(element.val().Cups)
+//         $('#sales').text(element.val().Sales)
+//         $('#cups').text(element.val().Cups)
+//     });
+// },{
+
+// });
+
+
+onValue(child(dbref, `Transactions/${theYear}/${theMonth+1}/${theDate}/1/`),(snapchat) => {
+    var cups = 0;
+    var sales = 0;
+    var trans = 0;
+    snapchat.forEach(element => {
+        element.forEach(data =>{
+            trans += 1;
+            data.forEach(data2 =>{
+                sales += parseInt(data2.val().productTotal.replace('₱ ', ''));
+                cups += parseInt(data2.val().productQty);
+            })
+        })
     });
+    $('#transactions').text(trans -2)
+    $('#cups').text(cups)
+    $('#sales').text(sales)
 },{
 
 });
-
 
 
 
